a84c7c81c47f03ac1992daa92a59daed837041e2
d10cn2btt <d10cn2btt@gmail.com>
2017-05-22 22:51:53 +0700

    add ng form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HeroFormComponent} from './hero-form/hero-form.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'app works11!';
 }
